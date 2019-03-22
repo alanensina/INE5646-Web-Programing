a7705f98a662212cb5a3b68c62e704a514bdda91
Alan Ensina <alanvinicius.ce@gmail.com>
2019-03-22 16:43:10 -0300

    Exercise 1 finished using filters

--- 01-app_pessoas/src/modelo.js
+++ 01-app_pessoas/src/modelo.js
@@ -4,16 +4,16 @@
  * Classe que representa uma pessoa.
  */
 class Pessoa {
-  constructor (nome, idade) {
+  constructor(nome, idade) {
     this.__nome = nome
     this.__idade = idade
   }
 
-  get idade () {
+  get idade() {
     return this.__idade
   }
 
-  get nome () {
+  get nome() {
     return this.__nome
   }
 }
@@ -24,27 +24,21 @@ class Pessoa {
  * @param number idadeLimite
  * @returns [Pessoa]
  */
-function selecionaPessoas (pessoas, idadeMinima) {
-  
-  var pessoas2 = []
-  
-  pessoas.forEach(element => {
-    if(element.idade > idadeMinima){
-      pessoas2.push(element)
-    }
-  });
-  
-  return pessoas2
+function selecionaPessoas(pessoas, idadeMinima) {
+
+  const filtroIdade = p => p.idade > idadeMinima
+
+  return pessoas.filter(filtroIdade)
 }
 
 // Dados fictícios
 const pessoas =
-  [ new Pessoa('Marcolino da Silva', 45),
-    new Pessoa('Tribulino Garrteto', 25),
-    new Pessoa('Burico do Pino Solto', 35),
-    new Pessoa('Marildo De Lusoneta', 29)
+  [new Pessoa('Marcolino da Silva', 45),
+  new Pessoa('Tribulino Garrteto', 25),
+  new Pessoa('Burico do Pino Solto', 35),
+  new Pessoa('Marildo De Lusoneta', 29)
   ]
 
 const idadeMinima = 30
 
-export {pessoas, idadeMinima, Pessoa, selecionaPessoas}
+export { pessoas, idadeMinima, Pessoa, selecionaPessoas }
