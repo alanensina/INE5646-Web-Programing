55d949a56cd0af5c625a26ae2431ed83525477a8
Alan Ensina <alanvinicius.ce@gmail.com>
2019-06-13 21:29:13 -0300

    deletando projeto anterior

--- 10-apps_compra_vende/app_compra/cliente/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.jsx'
-import 'bulma/css/bulma.min.css'
-
-ReactDOM.render(<App />, document.getElementById('root'))
